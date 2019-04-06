bec63e324723bfd43a0ec50d38b46c1a55abd49c
Shahriyar Shawon <ShahriyarShawon321@gmail.com>
2019-04-06 12:55:47 -0400

    checks first_time entry in account.json

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 from Accounts import Accounts
 import json
 
-# TODO: make this check accounts.json for first time boot or not
 def bootup(isFirstTime):
-    # with open()
-    if isFirstTime:
-        try:       
-            sign_in_or_sign_up = int(input("Press 1 to Sign In or 2 to Sign up\n>"))
-            if sign_in_or_sign_up == 1:
-                # sign in
-                pass
-            elif sign_in_or_sign_up == 2:
-                account.create_account()
-        except Exception as e:
-            print(e)
-    else:
-        # sign in
-        pass
+    with open("account.json","r") as f:
+        content = json.load(f)
+        isFirstTime = content["first_time"]
+
+        if isFirstTime:
+            content["first_time"] = False
+            f.seek(0)
+            json.dump(content, f, indent=4)
+            f.truncate()
+            try:       
+                sign_in_or_sign_up = int(input("Press 1 to Sign In or 2 to Sign up\n>"))
+                if sign_in_or_sign_up == 1:
+                    # sign in
+                    print("Sign in")
+                    pass
+                elif sign_in_or_sign_up == 2:
+                    account.create_account()
+            except Exception as e:
+                print(e)
+        else:
+            # sign in
+            print("Sign in")
+            pass
 
 
 def main():
