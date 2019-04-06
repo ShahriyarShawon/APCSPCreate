defdfd9a48c627214a3144dc19a9aa5946209a7d
Shahriyar Shawon <ShahriyarShawon321@gmail.com>
2019-04-06 16:38:05 -0400

    completed login

--- Accounts.py
+++ Accounts.py
@@ -1,16 +1,18 @@
 import json
 
 from CryptoTools import CryptoTools
+import DisplayData.DataDisplayer
 
 
 class Accounts:
     def __init__(self):
         self.crypt = CryptoTools()
-        print(self.crypt)
+        self.data_displayer = DataDisplayer() 
+        # print(self.crypt)
         
     def create_account(self):
-        username = raw_input("Enter a username: ")
-        password = raw_input("Enter a password: ")
+        username = input("Enter a username: ")
+        password = input("Enter a password: ")
         password = self.crypt.encrypt(password)
         
         with open("account.json","r+") as f:
@@ -29,26 +31,23 @@ class Accounts:
         with open("account.json","r") as f:
             content = json.load(f)
         if content["username"] == username and content["password"] == password:
-            # logged in
-            pass
-        # TODO: Make the sign in lead to something
+            self.data_displayer.display_entries()
         else:
             print("Sorry, either your username or pasword was not matched in the login ")
             # print("")
             fail_input = input("Press 1 to try again or 2 to create a new account")
             
-    def create_login(self,site,username,enc_password):
-        # with open("database.json","r+") as f:
-        #     content = json.load(f)
-        #     content[crypt.gen_rand_id(7)] = {"site":site,
-        #                                                 "username":username,
-        #                                                 "password":enc_password,
-        #                                                 }
-        #     f.seek(0)
-        #     json.dump(content, f, indent=4)
-        #     f.truncate()            
-        # pass
-        print(dir(self.crypt))
+    def create_entry(self,site,username,enc_password):
+        with open("database.json","r+") as f:
+            content = json.load(f)
+            content[self.crypt.gen_rand_id(8)] = {"site":site,
+                                            "username":username,
+                                            "password":enc_password,
+                                            }
+            f.seek(0)
+            json.dump(content, f, indent=4)
+            f.truncate()            
+        
             
     def delete_login(self,entry_id):
         with open("database.json","r+") as f:
@@ -59,5 +58,6 @@ class Accounts:
             f.truncate()            
         pass
 
-account = Accounts()
-account.create_login("asite.com","ausenrmae","apassword")
+# account = Accounts()
+# # account.create_entry("asite.com","ausername","apassword")
+# account.create_account()
